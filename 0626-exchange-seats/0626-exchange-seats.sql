ef95984fc2cd2e40f8f46dd1436b35dd7b71f7ca
Aneesh Jantikar <143446715+aneeshj-05@users.noreply.github.com>
2026-08-20 08:24:38 +0530

    Time: 404 ms (33.63%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select id,
+case when id%2=1 then coalesce(lead(student) over (order by id),student)
+else lag(student) over (order by id) end as student
+from Seat
+order by id
