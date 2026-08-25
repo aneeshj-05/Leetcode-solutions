1f22a88ebaa8ba3a60966d14618a69875e877278
Aneesh Jantikar <143446715+aneeshj-05@users.noreply.github.com>
2026-08-26 00:19:20 +0530

    Time: 363 ms (62.66%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,6 +1,10 @@
-SELECT e1.employee_id
-FROM Employees e1
-LEFT JOIN Employees e2
-    ON e1.manager_id = e2.employee_id
-WHERE e1.salary < 30000
-  AND e2.employee_id IS NULL;
+# Write your MySQL query statement below
+select employee_id
+from Employees
+where salary<30000
+and manager_id is not null
+and manager_id not in(
+    select employee_id
+    from Employees
+)
+order by employee_id;
